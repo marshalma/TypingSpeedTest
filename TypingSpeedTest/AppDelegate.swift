168e01c5ae8c93ca00b7c845be0efee2db00f145
Marshal Ma <mashuo93@gmail.com>
2015-05-05 19:18:24 +0800

    first commit

---
+++ TypingSpeedTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TypingSpeedTest
+//
+//  Created by 马朔 on 15/5/3.
+//  Copyright (c) 2015年 马朔. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TypingSpeedTest/ViewController.swift
@@ -0,0 +1,220 @@
+//
+//  ViewController.swift
+//  TypingSpeedTest
+//
+//  Created by 马朔 on 15/5/3.
+//  Copyright (c) 2015年 马朔. All rights reserved.
+//
+
+import Cocoa
+import Foundation
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        userTextField.editable = false
+        self.clearAll()
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        
+        super.init(coder: coder)
+        
+    }
+    
+    var timer : NSTimer? = nil
+    var userInTyping = false
+    var timeTotalUsed = 0 // as 0.1 second
+    var totalMistypedChar = 0
+    var standardTextString:String {
+        get{
+            return buildInTextField.stringValue
+        }
+    }
+    var userTypedTextString:String {
+        get{
+            return userTextField.stringValue
+        }
+        set{
+            userTextField.stringValue = newValue
+
+        }
+    }
+    var totalTimeConsumedString:String {
+        
+        get{
+            return ""
+        }
+        
+        set{
+            totalTime.stringValue = newValue + "s"
+        }
+    }
+    var correctCharString:String {
+        
+        get{
+            return ""
+        }
+        
+        set{
+            correctChar.stringValue = newValue
+        }
+    }
+    var totalCharString:String {
+        get{
+            return totalChar.stringValue
+        }
+        
+        set{
+            totalChar.stringValue = newValue
+        }
+    }
+    var wpmString:String {
+        get{
+            return ""
+        }
+        
+        set{
+            wpm.stringValue = newValue
+        }
+    }
+    var accuracyString:String {
+        get{
+            return ""
+        }
+        
+        set{
+            accuracy.stringValue = newValue + "%"
+        }
+    }
+    
+    @IBOutlet weak var totalTime: NSTextField!
+    @IBOutlet weak var correctChar: NSTextField!
+    @IBOutlet weak var totalChar: NSTextField!
+    @IBOutlet weak var wpm: NSTextField!
+    @IBOutlet weak var accuracy: NSTextField!
+    @IBOutlet weak var buildInTextField: NSTextField!
+    @IBOutlet weak var userTextField: NSTextField!
+    
+    
+    
+    
+    
+    func timerFireFunction() {
+        
+        self.timeTotalUsed += 1
+        
+        self.totalTimeConsumedString = String(format: "%.1f" ,Double(timeTotalUsed)/10.0)
+        
+    }
+
+    
+    // clear user textfield and
+    func clearAll() {
+        
+        self.timeTotalUsed = 0
+        self.totalMistypedChar = 0
+        self.totalTimeConsumedString = "0.0"
+        self.correctCharString = ""
+        self.totalCharString = String(count(standardTextString))
+        self.userTypedTextString = ""
+        
+    }
+    
+    override func keyDown(event: NSEvent) {
+        
+        if (!self.userInTyping) {
+            
+            return
+            
+        }
+        
+        if let char = event.characters {
+            //print(event)
+            
+            if (event.keyCode == 51) && (!userTypedTextString.isEmpty) {
+                
+                var userString = userTypedTextString
+                userString.removeAtIndex(userString.endIndex.predecessor())
+                userTypedTextString = userString
+                
+            }
+            else if(event.keyCode == 51){
+                
+            }
+            else{
+                userTypedTextString += char
+                
+                var typeCharAsStr = userTypedTextString.substringWithRange(userTypedTextString.endIndex.predecessor()..<userTypedTextString.endIndex)
+                var stdCharAsStr = standardTextString.substringWithRange(userTypedTextString.endIndex.predecessor()..<userTypedTextString.endIndex)
+                //println(stdCharAsStr + "vs" + typeCharAsStr)
+                if (typeCharAsStr != stdCharAsStr){
+                    totalMistypedChar += 1
+                    //println(totalMistypedChar)
+                }
+            }
+        }
+        
+        
+        //see if user have finished typing
+        if (count(standardTextString) == count(userTypedTextString)){
+            
+            stopTyping()
+            
+            self.userInTyping = false
+            
+        }
+    }
+    
+    func stopTyping() {
+        
+        timer?.invalidate()
+        
+        totalTimeConsumedString = String(format: "%.1f", Double(timeTotalUsed)/10.0)
+        
+        if let totalCharInt = totalCharString.toInt() {
+            
+            wpmString = String(format: "%.1f", Double(totalCharInt)*120.0/Double(timeTotalUsed))
+            
+            correctCharString = String((totalCharInt - totalMistypedChar)>0 ? (totalCharInt - totalMistypedChar) : 0)
+            
+            let rawAccuracy = Double(totalCharInt - totalMistypedChar)/Double(totalCharInt)
+            
+            accuracyString = String(format: "%.1f", rawAccuracy>0 ? rawAccuracy*100 : 0.0)
+            
+        }
+        
+    }
+
+    
+    
+    
+    @IBAction func startTyping(sender: NSButton) {
+        
+        self.clearAll()
+        
+        self.timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("timerFireFunction"), userInfo: nil, repeats: true)
+        
+        self.userInTyping = true
+        
+    }
+
+    @IBAction func stopTyping(sender: NSButton) {
+        
+        self.timer?.invalidate()
+        
+        self.clearAll()
+        
+        self.userInTyping = false
+        
+    }
+    
+}
+
